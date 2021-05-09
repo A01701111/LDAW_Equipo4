9185650b00534506427e71ca6e88fc7963ff3f95
Sebastian R <sebas.r.2599@gmail.com>
2021-05-09 00:51:05 -0500

    dashboard update

--- GameTrade/resources/views/dashboard.blade.php
+++ GameTrade/resources/views/dashboard.blade.php
@@ -1,4 +1,84 @@
 @extends('layouts.index')
 @section('content')
-    pp
+<div class="container-catalog-menu py-10">
+    <h1 class="item-menu-1 text-2xl text-gray-600">Games catalog</h1>
+
+    <button class="rounded-full main-dark flex items-center justify-center round-btn-48 shadow-2xl item-menu-2">
+        <span class="material-icons-outlined md-48">
+            add
+        </span>
+    </button>
+    <button class="rounded-full main-dark flex items-center justify-center round-btn-48 shadow-2xl item-menu-3">
+        <span class="material-icons-outlined md-48">
+            upload
+        </span>
+    </button>
+    <div class="wrap item-menu-4">
+        <div class="search">
+            <input type="text" class="searchTerm" placeholder="Search...">
+            <button type="submit" class="searchButton">
+                <span class="material-icons-outlined md-48">
+                    search
+                </span>
+            </button>
+        </div>
+    </div>
+</div>
+<div class="container-catalog py-16">
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+    <div class="catalog-card">
+        <img src="{{asset('storage/img/dummy1.jpg')}}" alt="" srcset="">
+        <div class="catalog-card-content">
+            Titulo de juego
+        </div>
+    </div>
+
+
+</div>
 @endsection
